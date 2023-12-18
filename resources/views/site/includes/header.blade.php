833df8b64e772e7ea330b73530c5ba8cf662d28a
Kareem Mohamed <kotbekareem74@gmail.com>
2023-12-18 10:59:42 -0500

    edits

--- resources/views/site/includes/header.blade.php
+++ resources/views/site/includes/header.blade.php
@@ -128,7 +128,7 @@
 			fill="currentColor"></path>
 	</svg></span>
     <div style="display: flex;flex-direction: column;justify-content: center;align-items: center;gap: 0;margin: 10px 3rem;">
-        <img alt="ثمانية" loading="lazy" decoding="async" data-nimg="1" class="!w-auto" style="color:transparent;vertical-align:middle;width:auto;margin-bottom: 30px;width: 160px !important;object-fit: contain;height: 50px !important;" src="http://127.0.0.1:8000/assets/img/logo-trans.png" width="90" height="50">
+        <img alt="ثمانية" loading="lazy" decoding="async" data-nimg="1" class="!w-auto" style="color:transparent;vertical-align:middle;width:auto;margin-bottom: 30px;width: 160px !important;object-fit: contain;height: 50px !important;" src="{{ asset("assets/img/logo-trans.png") }}" width="90" height="50">
         <h4 style="font-weight: 700;">ما هو حسابك؟</h4>
         <p>لتنشئ حسابًا أو تدخل بحسابك المسجل </p>
         <div class="form-group" style="margin: 20px 0 16px;width: 100%;">
@@ -142,7 +142,7 @@
 		<path d="M12 2C6.20156 2 1.5 6.70156 1.5 12.5C1.5 18.2984 6.20156 23 12 23C17.7984 23 22.5 18.2984 22.5 12.5C22.5 6.70156 17.7984 2 12 2ZM15.8766 16.4891L14.3297 16.482L12 13.7047L9.67266 16.4797L8.12344 16.4867C8.02031 16.4867 7.93594 16.4047 7.93594 16.2992C7.93594 16.2547 7.95234 16.2125 7.98047 16.1773L11.0297 12.5445L7.98047 8.91406C7.95215 8.87971 7.93643 8.8367 7.93594 8.79219C7.93594 8.68906 8.02031 8.60469 8.12344 8.60469L9.67266 8.61172L12 11.3891L14.3273 8.61406L15.8742 8.60703C15.9773 8.60703 16.0617 8.68906 16.0617 8.79453C16.0617 8.83906 16.0453 8.88125 16.0172 8.91641L12.9727 12.5469L16.0195 16.1797C16.0477 16.2148 16.0641 16.257 16.0641 16.3016C16.0641 16.4047 15.9797 16.4891 15.8766 16.4891Z" fill="currentColor"></path>
 	</svg></span>
     <div style="display: flex;flex-direction: column;justify-content: center;align-items: center;gap: 0;margin: 10px 3rem;">
-        <img alt="ثمانية" loading="lazy" decoding="async" data-nimg="1" class="!w-auto" style="color:transparent;vertical-align:middle;width:auto;margin-bottom: 30px;width: 160px !important;object-fit: contain;height: 50px !important;" src="http://127.0.0.1:8000/assets/img/logo-trans.png" width="90" height="50">
+        <img alt="ثمانية" loading="lazy" decoding="async" data-nimg="1" class="!w-auto" style="color:transparent;vertical-align:middle;width:auto;margin-bottom: 30px;width: 160px !important;object-fit: contain;height: 50px !important;" src="{{ asset("assets/img/logo-trans.png") }}" width="90" height="50">
         <h4 style="text-align: center;line-height: 25px;font-weight: 600;">
             أدخل الرمز الذي وصلك على <br>
             <span class="email-span"></span> لتنشئ حسابًا 
